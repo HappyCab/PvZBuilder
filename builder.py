58dbc39bd3a4a5b5fb9807f537fdae8e7d3be4d7
Kael <kaelk991@gmail.com>
2023-11-08 12:55:06 +1000

    Second Commit
    -Added Plants
    -Adjusted Directories

--- builder.py
+++ builder.py
@@ -44,8 +44,7 @@ class Tiles:
 class Tile:
     def __init__(self, id, x, y, tileType):
         self.id = id
-        self.x = int(x)
-        self.y = int(y)
+        self.x, self.y = int(x), int(y)
         self.tileType = tileType
 
         match tileType:
@@ -60,15 +59,76 @@ class Tile:
                 raise ValueError("Error! Tile Type: ", tileType)
 
         self.rect = pygame.Rect(self.x * TILESIZE, self.y * TILESIZE, TILESIZE, TILESIZE)
-        image_path = os.path.join("data", "images")
+        image_path = os.path.join("data", "images", "tiles")
         self.image = pygame.image.load(os.path.join(image_path, filename)).convert_alpha()
         self.image = pygame.transform.scale(self.image, (TILESIZE, TILESIZE))
 
     def debug_print(self):
-        s = "id: {}, x: {}, y: {}, kind {}"
-        s = s.format(self.id, self.x, self.y, self.tileType)
+        s = "id: {}, x: {}, y: {}, kind {}".format(self.id, self.x, self.y, self.tileType)
         print(s)
-                        
+
+class Plant:
+    def __init__(self, id, x, y, plantType):
+        self.id = id
+        self.x, self.y = int(x), int(y)
+        self.myinc = .05
+        self.plant_image = ""
+        match plantType:
+            case "m":
+                self.plant_image = "melonpult.png"                
+            case "w":
+                self.plant_image = "winterpult.png"                
+            case "g":
+                self.plant_image = "gloomshroom.png"              
+            case "c1":
+                self.plant_image = "cannon1.png"              
+            case "c2":
+                self.plant_image = "cannon2.png"              
+            case "u":
+                self.plant_image = "umbrella.png"               
+            case "s":
+                self.plant_image = "spikerock.png"
+            case _:
+                s = "Sorry I don't recognize that: {}".format(plantType)
+                raise ValueError(s)
+
+        image_path = os.path.join("data", "images", "plants")
+        self.image = pygame.image.load(os.path.join(image_path, self.plant_image)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (TILESIZE, TILESIZE))
+
+    def debug_print(self):
+        s = "id: {}, x: {}, y: {}".format(self.id, self.x, self.y)
+        print(s)
+        
+        
+class Plants:
+    def __init__(self, surface):
+        self.surface = surface
+        self.inner = []
+        self.current_plant = None
+
+        id = 0
+        filepath = os.path.join("data", "plant_map.txt")
+        with open(filepath, "r") as f:
+            mylines = f.readlines()
+            mylines = [i.strip() for i in mylines if len(i.strip()) > 0]
+        for count_i, line in enumerate(mylines):
+            for count_j, elem in enumerate(line):
+                if elem == "m":
+                    new_plant = Plant(id, count_j, count_i, elem)
+                    self.inner.append(new_plant)
+                    id += 1
+
+    def draw(self, surface):
+        if len(self.inner) == 0:
+            raise ValueError("Error: There are no plants to display")
+        for elem in self.inner:
+            myrect = pygame.Rect(elem.x *TILESIZE, elem.y * TILESIZE, TILESIZE, TILESIZE)
+            self.surface.blit(elem.image, myrect)
+
+    def debug_print(self):
+        for elem in self.inner:
+            elem.debug_print()
 
 
 class Game:
@@ -81,6 +141,7 @@ class Game:
         self.keep_looping = True
 
         self.tiles = Tiles(self.surface)
+        self.plants = Plants(self.surface)
 
     def events(self):
         for event in pygame.event.get():
@@ -96,6 +157,7 @@ class Game:
     def draw(self):
         self.surface.fill(self.BG_COLOUR)
         self.tiles.draw(self.surface)
+        self.plants.draw(self.surface)
         pygame.display.update()
 
     def main(self):
